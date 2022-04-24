fe02ef2853b255448c5555913ec8024967a46a51
GordonAly <twiwi@mail.ru>
2022-04-24 23:32:02 +0300

    lesson 2

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import products
 
 urlpatterns = [
-    path('', products),
+    path('', products, name='products'),
+    path('', products_all, name='все'),
+    path('', products_home, name='дом'),
+    path('', products_office, name='офис'),
+    path('', products_modern, name='модерн'),
+    path('', products_classic, name='классика')
+
 ]
